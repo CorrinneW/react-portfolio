f1a234616662823283c6e3d8b8d7e7c5a6d9688e
Corrinne W <corrinneworden@gmail.com>
2021-06-23 21:50:50 -0400

    resume page complete

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,16 +2,33 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Typography,
-    Box
+    Box,
+    Button,
 } from '@material-ui/core'
 
 import Container from '../Container'
 
+import resumePDF from '../../assets/corrinne_worden_resume_plain_text.pdf';
+
 const useStyles = makeStyles(theme => ({
     title: {
+        textAlign: "center"
+    },
+    titleHeader: {
         fontFamily: "'Permanent Marker', sans-serif",
         color: theme.palette.primary.main,
-        padding: '3rem 0',
+    },
+    button: {
+        marginTop: theme.spacing(5),
+        backgroundColor: theme.palette.darkAccent.main,
+        borderRadius: '1rem',
+        color: theme.palette.darkAccent.contrastText,
+        fontFamily: "'Staatliches', sans-serif",
+        fontSize: '1.5rem',
+        "&:hover, active": {
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText
+        }
     },
     heading: {
         color: theme.palette.secondary.main,
@@ -20,8 +37,9 @@ const useStyles = makeStyles(theme => ({
     subHeading: {
         color: theme.palette.secondary.main,
         padding: "0",
-        fontWeight: "bold"        
+        fontWeight: "bold"
     },
+    // controls the positioning of resume items based on breakpoints/order and creates pointer triangle
     item: {
         padding: "1rem",
         borderBottom: "2px solid #AD72A8",
@@ -48,7 +66,7 @@ const useStyles = makeStyles(theme => ({
             "&:nth-of-type(2n)": {
                 float: "right",
                 margin: "1rem",
-                borderColor: "#ffe8eb"
+                borderColor: "#AD72A8"
             },
             "&:nth-of-type(2n):before": {
                 right: "auto",
@@ -58,6 +76,7 @@ const useStyles = makeStyles(theme => ({
         }
 
     },
+    //controls style and positioning of year header based on breakpoints
     year: {
         textAlign: "center",
         maxWidth: "9.5rem",
@@ -84,6 +103,7 @@ const useStyles = makeStyles(theme => ({
             }
         }
     },
+    // creates center line and controls position based on break points
     timeLine: {
         position: "relative",
         padding: "1rem",
@@ -111,43 +131,54 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
+//open resumePDF in new window so that it can be downloaded or printed
+function handleClick() {
+ window.open(resumePDF, '_blank');
+}
+
 function Resume() {
     const classes = useStyles()
     return (
         <>
             <Container>
-                <Typography className={classes.title} variant="h4" align="center">
-                    Resume and Skills
-                </Typography>
-                <Box component="div" className={classes.timeLine}>
-                    <Typography className={`${classes.year} ${classes.item}`} variant="h2">
-                        2015
+                <Box component="div" className={classes.title}>
+                    <Typography className={classes.titleHeader} variant="h4">
+                        Resume and Skills
                     </Typography>
-                    <Box component="div" className={classes.item}>
-                        <Typography variant="h5"  className={classes.heading}>
-                            HTML & CSS
-                        </Typography>
-                        <Typography variant="body1"  className={classes.subHeading}>
-                            The Reynolds & Reynolds Company
-                        </Typography>
-                        <Typography variant="subtitle1" >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero finibus, suscipit nunc vel, eleifend felis. Nam posuere vestibulum mauris, quis pharetra nunc iaculis vel. Sed elementum nibh in tellus consectetur pretium. Aenean rutrum.
-                        </Typography>
-                    </Box>  
+                    <Button variant="contained" className={classes.button} onClick={handleClick}>
+                        View Full Resume
+                    </Button>
+                </Box>
+
+                <Box component="div" className={classes.timeLine}>
                     <Typography className={`${classes.year} ${classes.item}`} variant="h2">
                         2021
                     </Typography>
                     <Box component="div" className={classes.item}>
-                        <Typography variant="h5"  className={classes.heading}>
-                            position
+                        <Typography variant="h5" className={classes.heading}>
+                            Full Stack Web Development Bootcamp
                         </Typography>
-                        <Typography variant="body1"  className={classes.subHeading}>
-                            location, location
+                        <Typography variant="body1" className={classes.subHeading}>
+                            Ohio State University
                         </Typography>
                         <Typography variant="subtitle1" >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non libero finibus, suscipit nunc vel, eleifend felis. Nam posuere vestibulum mauris, quis pharetra nunc iaculis vel. Sed elementum nibh in tellus consectetur pretium. Aenean rutrum.
+                            A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Node JS, MySQL, MongoDB, Express, Handelbars.js & ReactJS.
                         </Typography>
-                    </Box>  
+                    </Box>
+                    <Typography className={`${classes.year} ${classes.item}`} variant="h2">
+                        2015
+                    </Typography>
+                    <Box component="div" className={classes.item}>
+                        <Typography variant="h5" className={classes.heading}>
+                            HTML & CSS
+                        </Typography>
+                        <Typography variant="body1" className={classes.subHeading}>
+                            The Reynolds & Reynolds Company
+                        </Typography>
+                        <Typography variant="subtitle1" >
+                            Responsible for the creation, editing and maintenance of Direct Mail Marketing and HTML email campaigns.
+                        </Typography>
+                    </Box>
                 </Box>
             </Container>
         </>
@@ -155,118 +186,3 @@ function Resume() {
 };
 
 export default Resume;
-
-
-
-// import React, { useState } from 'react';
-// import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-// import Container from '../Container';
-// import {
-//     Grid,
-//     Typography,
-//     Paper
-// } from "@material-ui/core";
-// import { makeStyles } from '@material-ui/core/styles';
-// import resumePDF from './corrinne_worden_resume_plain_text.pdf'
-
-// const useStyles = makeStyles(theme => ({
-//     centerText: {
-//         textAlign: "center"
-//     },
-//     title: {
-//         fontFamily: "'Permanent Marker', sans-serif",
-//         color: theme.palette.primary.main,
-//     },
-//     subtitle: {
-//         width: '100%',
-//         marginBottom: theme.spacing(2),
-//         borderBottom: '0.25rem dotted #8A533F',
-//         fontFamily: "'Staatliches', sans-serif",
-//         color: theme.palette.darkAccent.main,
-//         textAlign: 'left'
-//     },
-//     paper: {
-//         display: 'flex',
-//         flexDirection: 'column',
-//         textAlign: 'left',
-//         padding: theme.spacing(2),
-//         margin: theme.spacing(2),
-//         borderRadius: '1rem'
-//     },
-//     spacer: {
-//         borderRight: '0.25rem dotted #8A533F',
-//         height: theme.spacing(10)
-//     }
-// }));
-
-// function Resume() {
-//     const classes = useStyles()
-
-//     // const file = resumePDF
-//     // const [numPages, setNumPages] = useState(null);
-//     // const [pageNumber, setPageNumber] = useState(1);
-
-//     // function onDocumentLoadSuccess({ numPages }) {
-//     //     setNumPages(numPages);
-//     // }
-
-//     return (
-//         <div className={classes.centerText}>
-//             <Grid
-//                 container
-//                 direction="row"
-//                 justify="center"
-//                 align="center"
-//                 spacing={3}
-//             >
-//                 <Grid item xs={12}>
-//                     <Container>
-//                         <Typography className={classes.title} variant="h4">
-//                             Resume and Skills
-//                         </Typography>
-//                     </Container>
-//                 </Grid>
-
-//                 <Grid item xs={12}>
-//                     <Paper className={classes.paper}>
-//                         <div className={classes.subtitle}>
-//                             <Typography variant="h5">
-//                                 Summary
-//                             </Typography>
-//                         </div>
-//                         <Typography>
-//                             Full-stack developer with a graphic design background and a passion for creating more beautiful and accessible web experiences. Eager to leverage current skills and learn new ones with a creative, future-oriented team dedicated to building better web experiences for all.
-//                         </Typography>
-//                     </Paper>
-//                 </Grid>
-//                 <Grid item xs={12}>
-//                     <Paper className={classes.paper}>
-//                         <div className={classes.subtitle}>
-//                             <Typography variant="h5">
-//                                 Summary
-//                             </Typography>
-//                         </div>
-//                         <Typography>
-//                             Full-stack developer with a graphic design background and a passion for creating more beautiful and accessible web experiences. Eager to leverage current skills and learn new ones with a creative, future-oriented team dedicated to building better web experiences for all.
-//                         </Typography>
-//                     </Paper>
-//                 </Grid>
-//             </Grid>
-//         </div>
-//     )
-// }
-
-// export default Resume;
-
-
-{/* <div className={classes.pdf}>
-<Document
-    file={file}
-    onLoadSuccess={onDocumentLoadSuccess}
->
-    <Page pageNumber={pageNumber} />
-</Document>
-</div>
-<Typography>
-Page {pageNumber} of {numPages}
-</Typography> */}
